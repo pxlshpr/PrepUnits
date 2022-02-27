8ae7571c50bedd5045b5aa9d6b2e884baf485a9d
Ahmed Khalaf <pxlshpr@gmail.com>
2022-02-27 12:23:14 +0500

    tag increment

---
+++ Sources/PrepUnits/WeightUnit.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+enum WeightUnit: Int16, CaseIterable {
+
+    case g = 1
+    case kg
+    case oz
+    case lb
+    case mg
+    
+    var regex: String {
+        switch self {
+        case .g:
+            return #"^(g raw weight|gramm|gram|gr|gm|g)"#
+        case .kg:
+            return #"^kg"#
+        case .mg:
+            return #"^mg"#
+        case .oz:
+            return #"^(wt. oz|ounce|oz)"#
+        case .lb:
+            return #"(pound|lb)"#
+        }
+    }
+}
