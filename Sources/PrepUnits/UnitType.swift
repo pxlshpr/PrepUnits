e80645a8b00a5ea63fdd7e4a3718a7394ad8d53d
Ahmed Khalaf <pxlshpr@gmail.com>
2022-02-28 16:05:28 +0500

    tag increment

---
+++ Sources/PrepUnits/UnitType.swift
@@ -0,0 +1,48 @@
+import Foundation
+
+public enum UnitType: Int16, CaseIterable {
+    case weight = 1
+    case volume
+    case serving
+    case size
+}
+
+public extension UnitType {
+    
+    var description: String {
+        switch self {
+        case .weight:
+            return "Weight"
+        case .volume:
+            return "Volume"
+        case .serving:
+            return "Serving"
+        case .size:
+            return "Size"
+        }
+    }
+    
+    var isMeasurement: Bool {
+        self == .weight || self == .volume
+    }
+    
+    var oppositeMeasurement: UnitType? {
+        if self == .weight { return .volume }
+        if self == .volume { return .weight }
+        return nil
+    }
+
+    init?(_ nutrientsString: String) {
+        switch nutrientsString {
+        case "g":
+            self = .weight
+        case "mL":
+            self = .volume
+        case "serving":
+            self = .serving
+        default:
+            return nil
+        }
+    }    
+}
+
