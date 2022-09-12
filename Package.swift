27ee9abebde1f6c6bd3c0049568655f34b6c11e9
Ahmed Khalaf <pxlshpr@gmail.com>
2022-09-12 09:27:22 +0500

    tag increment

--- Package.swift
+++ Package.swift
@@ -18,6 +18,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/pxlshpr/SwiftSugar", from: "0.0.1"),
+        .package(url: "https://github.com/pxlshpr/SwiftUISugar", from: "0.0.168"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -25,7 +26,8 @@ let package = Package(
         .target(
             name: "PrepUnits",
             dependencies: [
-                .product(name: "SwiftSugar", package: "swiftsugar")
+                .product(name: "SwiftSugar", package: "swiftsugar"),
+                .product(name: "SwiftUISugar", package: "swiftuisugar"),
             ]),
         .testTarget(
             name: "PrepUnitsTests",
