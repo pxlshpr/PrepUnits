bbc6ec17995c034735e14352d34453bbf96823e9
Ahmed Khalaf <pxlshpr@gmail.com>
2022-09-17 08:34:51 +0500

    tag increment

---
+++ Sources/PrepUnits/ReferenceValues/MacroRDA.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+//TODO: Revisit these
+/// RDA values for macros
+public struct MacroRDA {
+    public static let protein = 50.0
+    public static let carb = 78.0
+    public static let fat = 275.0
+}
