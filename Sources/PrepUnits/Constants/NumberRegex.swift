f4f98bc41f16dcb82f1bc046817c5f30b8e4f8c6
Ahmed Khalaf <pxlshpr@gmail.com>
2022-10-04 10:27:59 +0500

    tag increment

--- Sources/PrepUnits/Constants/NumberRegex.swift
+++ Sources/PrepUnits/Constants/NumberRegex.swift
@@ -4,7 +4,7 @@ import Foundation
 public struct NumberRegex {
     /// Recognizes number in a string using comma as decimal place (matches `39,3` and `2,05` but not `2,000` or `1,2,3`)
 //    static let usingCommaAsDecimalPlace = #"^[0-9]*,[0-9][0-9]?([^0-9]|$)"#
-    static let usingCommaAsDecimalPlace = #"^[0-9]*,[0-9][0-9]*([^0-9]|$)"#
-    static let isFraction = #"^([0-9]+)\/([0-9]+)"#
+    public static let usingCommaAsDecimalPlace = #"^[0-9]*,[0-9][0-9]*([^0-9]|$)"#
+    public static let isFraction = #"^([0-9]+)\/([0-9]+)"#
 }
 
