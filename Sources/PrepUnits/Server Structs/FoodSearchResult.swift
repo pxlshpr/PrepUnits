83deea1d8e83b2fbb63eb8384a37b55ee67cd0cd
Ahmed Khalaf <pxlshpr@gmail.com>
2022-10-24 21:34:14 +0500

    tag increment

--- Sources/PrepUnits/Server Structs/FoodSearchResult.swift
+++ Sources/PrepUnits/Server Structs/FoodSearchResult.swift
@@ -6,12 +6,28 @@ public struct FoodSearchResult: Codable, Identifiable, Hashable, Equatable {
     public let emoji: String
     public let detail: String?
     public let brand: String?
+    
+    public let carb: Double
+    public let fat: Double
+    public let protein: Double
 
-    public init(id: UUID, name: String, emoji: String, detail: String? = nil, brand: String? = nil) {
+    public init(
+        id: UUID,
+        name: String,
+        emoji: String,
+        detail: String? = nil,
+        brand: String? = nil,
+        carb: Double,
+        fat: Double,
+        protein: Double
+    ) {
         self.id = id
         self.name = name
         self.emoji = emoji
         self.detail = detail
         self.brand = brand
+        self.carb = carb
+        self.fat = fat
+        self.protein = protein
     }
 }
