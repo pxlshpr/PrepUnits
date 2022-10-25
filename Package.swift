dff19b6ed91d395d1254a3696c825f0d12ad9e1a
Ahmed Khalaf <pxlshpr@gmail.com>
2022-10-25 21:52:11 +0500

    bump

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["PrepUnits"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/pxlshpr/SwiftSugar", from: "0.0.69"),
+        .package(url: "https://github.com/pxlshpr/SwiftSugar", from: "0.0.74"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
--- Sources/PrepUnits/Server Structs/PostParams.swift
+++ Sources/PrepUnits/Server Structs/PostParams.swift
@@ -1,17 +1,17 @@
-import Foundation
-
-public struct PostParamsFoodsForIds: Codable {
-    public let ids: [UUID]
-    
-    public init(ids: [UUID]) {
-        self.ids = ids
-    }
-}
-
-public struct PostParamsBarcodes: Codable {
-    public let payloads: [String]
-    
-    public init(payloads: [String]) {
-        self.payloads = payloads
-    }
-}
+//import Foundation
+//
+//public struct PostParamsFoodsForIds: Codable {
+//    public let ids: [UUID]
+//    
+//    public init(ids: [UUID]) {
+//        self.ids = ids
+//    }
+//}
+//
+//public struct PostParamsBarcodes: Codable {
+//    public let payloads: [String]
+//    
+//    public init(payloads: [String]) {
+//        self.payloads = payloads
+//    }
+//}
